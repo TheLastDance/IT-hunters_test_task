932dc6af341c352491121b20705353544765f15e
unknown <davit.khachaturov.1@iliauni.edu.ge>
2023-10-01 21:58:55 +0400

    changed popup

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ const signUp = document.querySelector('#sign_up');
 const body = document.querySelector('body');
 const burger = document.querySelector('#burger_field');
 const burgerNav = document.querySelector('#burger_navigation');
+const register = document.querySelector('#register_overlay');
+const close_btn = document.querySelector('#close_btn');
 
 burger.addEventListener('click', () => {
   burger.classList.toggle('active');
@@ -12,13 +14,16 @@ burger.addEventListener('click', () => {
   burgerNav.classList.toggle('open');
 })
 
+close_btn.addEventListener('click', () => {
+  register.classList.toggle("active");
+})
+
 signUp.addEventListener('click', (e) => {
   e.preventDefault();
-  popup.style.opacity = "1";
-  popup.style.display = 'block';
   burger.classList.remove('active')
   burgerNav.classList.remove('open');
   body.classList.remove("noScroll");
+  register.classList.toggle("active");
 })
 
 document.onreadystatechange = function () {
